60edcc4bcdefe42648ce8e6d12ff0fd5eba37178
Kate Shlykova <e2.shlykova@itransition.com>
2022-08-06 14:58:06 +0300

    feat: add uath module to users module

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,13 +1,10 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto {
-  @ApiProperty({ example: 'userLogin', description: 'Логин пользователя' })
   @IsNotEmpty()
   @IsString()
   login: string;
 
-  @ApiProperty({ example: '12345678', description: 'Пароль пользователя' })
   @IsNotEmpty()
   @IsString()
   password: string;
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,32 +10,35 @@ import {
   UseInterceptors,
   ClassSerializerInterceptor,
   HttpCode,
+  UseGuards,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdatePasswordDto } from './dto/update-password.dto';
 import { validate as uuidValidate } from 'uuid';
-import { DataSource } from 'typeorm';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('/user')
 @UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
   constructor(
-    private userService: UsersService,
-    private dataSource: DataSource,
+    private userService: UsersService
   ) {}
 
   @UseInterceptors(ClassSerializerInterceptor)
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.userService.findAll();
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get(':id')
   findOne(@Param('id') id: string) {
     if (!uuidValidate(id)) {
@@ -44,6 +47,7 @@ export class UsersController {
     return this.userService.findOne(id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put(':id')
   update(
     @Param('id') id: string,
@@ -55,6 +59,7 @@ export class UsersController {
     return this.userService.update(id, updatePasswordDto);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Delete(':id')
   @HttpCode(204)
   remove(@Param('id') id: string) {
